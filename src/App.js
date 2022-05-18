5b08243a94fa21225c85065719530e033f06150d
anjalireghunath <anjalireghu98@gmail.com>
2022-05-18 13:52:03 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import AddPost from './components/AddPost';
 import SearchPost from './components/SearchPost';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
    <div>
-<AddPost/>
-<SearchPost/>
+<BrowserRouter>
+<Routes>
+  <Route path='/' exact element={<AddPost/>}/>
+  <Route path='/search' exact element={<SearchPost/>}/>
+</Routes>
+</BrowserRouter>
    </div>
   );
 }
