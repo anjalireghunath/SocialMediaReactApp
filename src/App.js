f51a5a994eb60062cb1fdf79f188d99046220a54
anjalireghunath <anjalireghu98@gmail.com>
2022-05-18 12:49:49 +0530

    add post

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import AddPost from './components/AddPost';
 function App() {
   return (
    <div>
-
+<AddPost/>
    </div>
   );
 }
